f880e5463c4c68f851f112881356be555a536e80
Sanjana Sankholkar <sanjan.sankholkar@gmail.com>
2019-12-16 17:53:47 -0700

    added to the help page

--- calculator.js
+++ calculator.js
@@ -1,7 +1,22 @@
 function display_grades(){
-  var curr_grade = document.getElementById('current_grade').value;
+  var current_grade = document.getElementById('current_grade').value;
   var wanted_grade = document.getElementById('wanted_grade').value;
   var final_worth = document.getElementById('final_worth').value;
-  var result = (wanted_grade-(curr_grade*(1-(final_worth/100))))/(final_worth/100)
-  document.getElementById('answer').innerHTML =result.toFixed(2);
+  var result = (wanted_grade-(current_grade*(1-(final_worth/100))))/(final_worth/100)
+  document.getElementById('answer').innerHTML = "You need to get a " + result.toFixed(2) + "% to achieve your wanted grade.";
+}
+
+function point_system(){
+  var tot_points = document.getElementById('tot_points').value;
+  var fin_points = document.getElementById('fin_points').value;
+  var result = ((fin_points/tot_points)*100)
+  document.getElementById('answer1').innerHTML = "Your final is worth " + result.toFixed(2) + "% of your grade."
+}
+
+function more_grades(){
+  var curr_grade = document.getElementById('curr_grade').value;
+  var fin_grade = document.getElementById('fin_grade').value;
+  var fin_worth = document.getElementById('fin_worth').value;
+  var result = ((curr_grade*(1-(fin_worth/100)))+(fin_grade*(fin_worth/100)))
+  document.getElementById('ans').innerHTML = "Your final grade in that class will be a " + result.toFixed(2) + "%."
 }
--- help.js
+++ help.js
@@ -15,3 +15,165 @@ function ready(){
     }
   }
 };
+
+// Math
+function AddMathTips(){
+  let mathtips = document.getElementById('mathtips');
+  mathtips.classList.remove('hide_matht');
+}
+
+function AcceptMathTips(){
+  let mathtips = document.getElementById('mathtips');
+  let math_original_tips = document.getElementById('math_tips_text').innerHTML;
+  let math_input = document.getElementById('math_input').value;
+  document.getElementById('math_tips_text').innerHTML = math_original_tips + math_input + "<br>"
+  mathtips.classList.add('hide_matht');
+}
+
+function AddMathResources(){
+  let mathresources = document.getElementById('mathresources');
+  mathresources.classList.remove('hide_mathr');
+}
+
+function AcceptMathResources(){
+  let mathresources = document.getElementById('mathresources');
+  let math_original_resources = document.getElementById('math_resources_text').innerHTML;
+  let math_input = document.getElementById('math_input2').value;
+  document.getElementById('math_resources_text').innerHTML = math_original_resources + math_input + "<br>"
+  mathresources.classList.add('hide_mathr');
+}
+
+// Science
+function AddScienceTips(){
+  let sciencetips = document.getElementById('sciencetips');
+  sciencetips.classList.remove('hide_sciencet');
+}
+
+function AcceptScienceTips(){
+  let sciencetips = document.getElementById('sciencetips');
+  let science_original_tips = document.getElementById('science_tips_text').innerHTML;
+  let science_input = document.getElementById('science_input').value;
+  document.getElementById('science_tips_text').innerHTML = science_original_tips + science_input + "<br>"
+  sciencetips.classList.add('hide_sciencet');
+}
+
+function AddScienceResources(){
+  let scienceresources = document.getElementById('scienceresources');
+  scienceresources.classList.remove('hide_sciencer');
+}
+
+function AcceptScienceResources(){
+  let scienceresources = document.getElementById('scienceresources');
+  let science_original_resources = document.getElementById('science_resources_text').innerHTML;
+  let science_input = document.getElementById('science_input2').value;
+  document.getElementById('science_resources_text').innerHTML = science_original_resources + science_input + "<br>"
+  scienceresources.classList.add('hide_sciencer');
+}
+
+// ELA
+function AddELATips(){
+  let elatips = document.getElementById('elatips');
+  elatips.classList.remove('hide_elat');
+}
+
+function AcceptELATips(){
+  let elatips = document.getElementById('elatips');
+  let ela_original_tips = document.getElementById('ela_tips_text').innerHTML;
+  let ela_input = document.getElementById('ela_input').value;
+  document.getElementById('ela_tips_text').innerHTML = ela_original_tips + ela_input + "<br>"
+  elatips.classList.add('hide_elat');
+}
+
+function AddELAResources(){
+  let elaresources = document.getElementById('elaresources');
+  elaresources.classList.remove('hide_elar');
+}
+
+function AcceptELAResources(){
+  let elaresources = document.getElementById('elaresources');
+  let ela_original_resources = document.getElementById('ela_resources_text').innerHTML;
+  let ela_input = document.getElementById('ela_input2').value;
+  document.getElementById('ela_resources_text').innerHTML = ela_original_resources + ela_input + "<br>"
+  elaresources.classList.add('hide_elar');
+}
+
+// Social Studies
+function AddSStudiesTips(){
+  let sstudiestips = document.getElementById('sstudiestips');
+  sstudiestips.classList.remove('hide_sstudiest');
+}
+
+function AcceptSStudiesTips(){
+  let sstudiestips = document.getElementById('sstudiestips');
+  let sstudies_original_tips = document.getElementById('sstudies_tips_text').innerHTML;
+  let sstudies_input = document.getElementById('sstudies_input').value;
+  document.getElementById('sstudies_tips_text').innerHTML = sstudies_original_tips + sstudies_input + "<br>"
+  sstudiestips.classList.add('hide_sstudiest');
+}
+
+function AddSStudiesResources(){
+  let sstudiesresources = document.getElementById('sstudiesresources');
+  sstudiesresources.classList.remove('hide_sstudiesr');
+}
+
+function AcceptSStudiesResources(){
+  let sstudiesresources = document.getElementById('sstudiesresources');
+  let sstudies_original_resources = document.getElementById('sstudies_resources_text').innerHTML;
+  let sstudies_input = document.getElementById('sstudies_input2').value;
+  document.getElementById('sstudies_resources_text').innerHTML = sstudies_original_resources + sstudies_input + "<br>"
+  sstudiesresources.classList.add('hide_sstudiesr');
+}
+
+// Languages
+function AddLanguageTips(){
+  let languagetips = document.getElementById('languagetips');
+  languagetips.classList.remove('hide_languaget');
+}
+
+function AcceptLanguageTips(){
+  let languagetips = document.getElementById('languagetips');
+  let language_original_tips = document.getElementById('language_tips_text').innerHTML;
+  let language_input = document.getElementById('language_input').value;
+  document.getElementById('language_tips_text').innerHTML = language_original_tips + language_input + "<br>"
+  languagetips.classList.add('hide_languaget');
+}
+
+function AddLanguageResources(){
+  let languageresources = document.getElementById('languageresources');
+  languageresources.classList.remove('hide_languager');
+}
+
+function AcceptLanguageResources(){
+  let languageresources = document.getElementById('languageresources');
+  let language_original_resources = document.getElementById('language_resources_text').innerHTML;
+  let language_input = document.getElementById('language_input2').value;
+  document.getElementById('language_resources_text').innerHTML = language_original_resources + language_input + "<br>"
+  languageresources.classList.add('hide_languager');
+}
+
+// Other
+function AddOtherTips(){
+  let othertips = document.getElementById('othertips');
+  othertips.classList.remove('hide_othert');
+}
+
+function AcceptOtherTips(){
+  let othertips = document.getElementById('othertips');
+  let other_original_tips = document.getElementById('other_tips_text').innerHTML;
+  let other_input = document.getElementById('other_input').value;
+  document.getElementById('other_tips_text').innerHTML = other_original_tips + other_input + "<br>"
+  othertips.classList.add('hide_othert');
+}
+
+function AddOtherResources(){
+  let otherresources = document.getElementById('otherresources');
+  otherresources.classList.remove('hide_otherr');
+}
+
+function AcceptOtherResources(){
+  let otherresources = document.getElementById('otherresources');
+  let other_original_resources = document.getElementById('other_resources_text').innerHTML;
+  let other_input = document.getElementById('other_input2').value;
+  document.getElementById('other_resources_text').innerHTML = other_original_resources + other_input + "<br>"
+  otherresources.classList.add('hide_otherr');
+}
